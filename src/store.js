00b96963bbc8c869a46f8872feb6e57e0a62753b
uprajnambhat <uprajnambhat96@gmail.com>
2024-07-07 12:32:44 +0530

    Updates

--- src/store.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers, createStore } from "redux";
-import { menuReducers } from "./reducers/menuReducers.js";
-
-const foodItemsReducer = combineReducers({
-  menuDetails: menuReducers,
-});
-
-const store = createStore(foodItemsReducer);
-export default store;
